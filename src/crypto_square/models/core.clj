a9e3bc33003dfeaa1f3f6bbf03a5bd55d876ac62
Andy Marks <amarks@thoughtworks.com>
2015-05-19 15:13:26 +1000

    Now accepting a map back from service

--- src/crypto_square/models/core.clj
+++ src/crypto_square/models/core.clj
@@ -4,7 +4,10 @@
 		[cheshire.core :as json]))
  
 (defn ciphertext [plaintext]
-	(:body (client/post "http://localhost:3000"
-		{:body (json/generate-string {:plaintext plaintext})
-	   :content-type :json
-  	 :accept :json})))
+	(let [response (client/post "http://localhost:3000"
+					{:body (json/generate-string {:plaintext plaintext})
+	   			 :content-type :json
+  	 			 :accept :json})
+				body (:body response)
+				json-body (json/parse-string body)]
+		(get json-body "ciphertext")))
