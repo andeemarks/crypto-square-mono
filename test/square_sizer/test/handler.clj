708d0652a0e06885ffe74921b680d7e75a21ade4
Andy Marks <amarks@thoughtworks.com>
2015-05-19 17:37:27 +1000

    Tightening up on assertions

--- test/square_sizer/test/handler.clj
+++ test/square_sizer/test/handler.clj
@@ -1,21 +1,25 @@
 (ns square-sizer.test.handler
   (:use clojure.test
         ring.mock.request
+        cheshire.core        
         square-sizer.handler))
 
 (deftest test-app
   (testing "handles empty input gracefully"
-    (let [response (app (request :get "/"))]
+    (let [response (app (request :get "/"))
+          body (parse-string (:body response))]
       (is (= (:status response) 200))
-      (is (.contains (:body response) "0"))))
+      (is (= (get body "size") 0))))
 
   (testing "size when plaintext length is exact square"
-    (let [response (app (request :get "/abcd"))]
+    (let [response (app (request :get "/abcd"))
+          body (parse-string (:body response))]
       (is (= (:status response) 200))
-      (is (.contains (:body response) "2"))))
+      (is (= (get body "size") 2))))
 
   (testing "size when plaintext length is not square"
-    (let [response (app (request :get "/abcde"))]
+    (let [response (app (request :get "/abcde"))
+          body (parse-string (:body response))]
       (is (= (:status response) 200))
-      (is (.contains (:body response) "3")))))
+      (is (= (get body "size") 3)))))
 
