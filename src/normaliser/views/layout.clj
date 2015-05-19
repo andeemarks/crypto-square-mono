4ab8acc8b3836a35a94daa18747148cb37cda13f
Andy Marks <amarks@thoughtworks.com>
2015-05-19 16:54:44 +1000

    Initial commit

---
+++ src/normaliser/views/layout.clj
@@ -0,0 +1,9 @@
+(ns normaliser.views.layout
+  (:require [hiccup.page :refer [html5 include-css]]))
+
+(defn common [& body]
+  (html5
+    [:head
+     [:title "Welcome to normaliser"]
+     (include-css "/css/screen.css")]
+    [:body body]))
