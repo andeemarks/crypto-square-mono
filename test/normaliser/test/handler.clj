3d9934d2e5890c1392629967344743b53075dc70
Andy Marks <amarks@thoughtworks.com>
2015-05-19 17:40:41 +1000

    Tightening up on test assertions

--- test/normaliser/test/handler.clj
+++ test/normaliser/test/handler.clj
@@ -2,25 +2,30 @@
   (:use clojure.test
         ring.mock.request
         normaliser.handler
+        cheshire.core
         ring.util.codec))
 
 (deftest test-app
   (testing "already normal text is unchanged"
-    (let [response (app (request :get "/abcd1234"))]
+    (let [response (app (request :get "/abcd1234"))
+          body (parse-string (:body response))]
       (is (= (:status response) 200))
-      (is (.contains (:body response) "abcd1234"))))
+      (is (.equals (get body "normalised-text") "abcd1234"))))
 
   (testing "everything is down cased"
-    (let [response (app (request :get "/aBcDe"))]
+    (let [response (app (request :get "/aBcDe"))
+          body (parse-string (:body response))]
       (is (= (:status response) 200))
-      (is (.contains (:body response) "abcde"))))
+      (is (.equals (get body "normalised-text") "abcde"))))
 
   (testing "punctuation is removed"
-    (let [response (app (request :get (str "/" (url-encode "a{}bcd']12#$%3&4"))))]
+    (let [response (app (request :get (str "/" (url-encode "a{}bcd']12#$%3&4"))))
+          body (parse-string (:body response))]
       (is (= (:status response) 200))
-      (is (.contains (:body response) "abcd1234"))))
+      (is (.equals (get body "normalised-text") "abcd1234"))))
 
   (testing "spaces are removed"
-    (let [response (app (request :get "/a+b+c+d"))]
+    (let [response (app (request :get "/a+b+c+d"))
+          body (parse-string (:body response))]
       (is (= (:status response) 200))
-      (is (.contains (:body response) "abcd")))))
+      (is (.equals (get body "normalised-text") "abcd")))))
