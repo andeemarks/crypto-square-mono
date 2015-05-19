16b5d635077d807036fd1ed1ca333ca1d484765e
Andy Marks <amarks@thoughtworks.com>
2015-05-19 15:17:14 +1000

    Refactoring

--- src/crypto_square/models/core.clj
+++ src/crypto_square/models/core.clj
@@ -3,11 +3,14 @@
 		[clj-http.client :as client]
 		[cheshire.core :as json]))
  
+(defn- ciphertext-request [plaintext]
+	(client/post 
+		"http://localhost:3000"
+		{:body (json/generate-string {:plaintext plaintext})
+		 :content-type :json
+		 :accept :json}))
+
 (defn ciphertext [plaintext]
-	(let [response (client/post "http://localhost:3000"
-					{:body (json/generate-string {:plaintext plaintext})
-	   			 :content-type :json
-  	 			 :accept :json})
-				body (:body response)
-				json-body (json/parse-string body)]
+	(let [response (ciphertext-request plaintext)
+				json-body (json/parse-string (:body response))]
 		(get json-body "ciphertext")))
