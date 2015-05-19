de247a4a6926d143b8ae4057e131d7bad6d060b9
Andy Marks <amarks@thoughtworks.com>
2015-05-19 11:10:27 +1000

    Now POSTing to service to generate ciphertext

--- src/crypto_square/models/core.clj
+++ src/crypto_square/models/core.clj
@@ -1,42 +1,10 @@
 (ns crypto-square.models.core
-	(:require [clojure.string :as clj-str])
-	(:require [clj-http.client :as client]))
+	(:require 
+		[clj-http.client :as client]
+		[cheshire.core :as json]))
  
-(defn- no-punctuation [c]
-  (or (Character/isLetter c)
-      (Character/isDigit c)))
- 
-(defn- remove-punctuation [text]
-  (clj-str/join "" (filter no-punctuation text)))
- 
-(defn normalize-plaintext [text]
-  (clj-str/lower-case (remove-punctuation text)))
- 
-(defn square-size [text]
-  (int (Math/ceil (Math/sqrt (count text)))))
- 
-(defn plaintext-segments [text]
-  (let [normalized-text (normalize-plaintext text)
-        segment-size (square-size normalized-text)]
-    (map clj-str/join (partition-all segment-size normalized-text))))
- 
-(defn- pad-segments [segments segments-size]
-  (map #(format (str "%-" segments-size "s") %) segments))
- 
-(defn- segments-in-columns [text]
-  (let [segments (plaintext-segments text)
-        segment-size (count (first segments))]
-    (apply map
-           #(clj-str/trim (apply str %&))
-           (pad-segments segments segment-size))))
- 
-(defn- remove-spaces [text]
-  (clj-str/replace text " " ""))
- 
-(defn normalize-ciphertext [text]
-  (->> text
-       segments-in-columns
-       (clj-str/join " ")))
- 
-(defn ciphertext [text]
-	(:body (client/get (str "http://localhost:3000/" text))))
+(defn ciphertext [plaintext]
+	(:body (client/post "http://localhost:3000"
+		{:body (json/generate-string {:plaintext plaintext})
+	   :content-type :json
+  	 :accept :json})))
