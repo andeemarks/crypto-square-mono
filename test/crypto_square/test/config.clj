7feed2268eedd234f467c95c59833fcdf226a941
Andy Marks <amarks@thoughtworks.com>
2015-05-23 20:47:03 +1000

    Refactoring

--- test/crypto_square/test/config.clj
+++ test/crypto_square/test/config.clj
@@ -1,5 +1,34 @@
-(ns crypto-square.test.config)
+(ns crypto-square.test.config
+  (:require [ring.adapter.jetty :refer [run-jetty]]
+            [clj-webdriver.taxi :refer :all]
+            [crypto-square.routes.home :refer [home-routes]]))
 
 (def test-port 5744)
 (def test-host "localhost")
 (def test-base-url (str "http://" test-host ":" test-port "/"))
+
+(defn start-server []
+  (loop [server (run-jetty home-routes {:port test-port, :join? false})]
+    (if (.isStarted server)
+      server
+      (recur server))))
+
+(defn stop-server [server]
+  (.stop server))
+
+(defn start-browser []
+  (set-driver! {:browser :chrome}))
+
+(defn stop-browser []
+  (quit))
+
+(defn with-server [t]
+  (let [server (start-server)]
+    (t)
+    (stop-server server)))
+
+(defn with-browser [t]
+  (System/setProperty "webdriver.chrome.driver" (str (System/getenv "HOME") "/bin/chromedriver"))
+  (start-browser)
+  (t)
+  (stop-browser))
--- test/crypto_square/test/handler.clj
+++ test/crypto_square/test/handler.clj
@@ -1,35 +1,7 @@
 (ns crypto-square.test.handler
   (:require [clojure.test :refer :all]
-            [ring.adapter.jetty :refer [run-jetty]]
             [clj-webdriver.taxi :refer :all]
-            [crypto-square.test.config :refer :all]
-            [crypto-square.routes.home :refer [home-routes]]))
-
-(defn start-server []
-  (loop [server (run-jetty home-routes {:port test-port, :join? false})]
-    (if (.isStarted server)
-      server
-      (recur server))))
-
-(defn stop-server [server]
-  (.stop server))
-
-(defn start-browser []
-  (set-driver! {:browser :chrome}))
-
-(defn stop-browser []
-  (quit))
-
-(defn with-server [t]
-  (let [server (start-server)]
-    (t)
-    (stop-server server)))
-
-(defn with-browser [t]
-  (System/setProperty "webdriver.chrome.driver" (str (System/getenv "HOME") "/bin/chromedriver"))
-  (start-browser)
-  (t)
-  (stop-browser))
+            [crypto-square.test.config :refer :all]))
 
 (use-fixtures :once with-server with-browser)
 
