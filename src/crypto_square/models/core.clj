b7e3471b8c8a66b1743df1f0724981320893022a
Andy Marks <amarks@thoughtworks.com>
2015-05-24 12:25:52 +1000

    Now sending corr-id to riemann

--- src/crypto_square/models/core.clj
+++ src/crypto_square/models/core.clj
@@ -7,26 +7,27 @@
 
 (defn- corr-id [] (str "crypto-square-" (java.util.UUID/randomUUID)))
 
-(defn- ciphertext-request [plaintext]
+(defn- ciphertext-request [plaintext corr-id]
 	(client/post 
 		"http://localhost:3000"
 		{:body (json/generate-string {:plaintext plaintext})
 		 :content-type :json
-		 :headers {"X-Correlation-Id" (corr-id)}
+		 :headers {"X-Correlation-Id" corr-id}
 		 :accept :json}))
 
-(defn- send-event [plaintext]
+(defn- send-event [plaintext corr-id]
 	(try
 		(let [c (riemann/tcp-client {:host "127.0.0.1"})]
 	        (riemann/send-event c
-	                {:service "crypto-square" :description plaintext})
+	                {:service "crypto-square" :description corr-id})
 	        (riemann/close-client c))
 		(catch java.io.IOException ex 
 			(log/warn "Cannot find Riemann!"))))
 
 (defn ciphertext [plaintext]
-	(let [response (ciphertext-request plaintext)
+	(let [correlation-id (corr-id) 
+		 		response (ciphertext-request plaintext correlation-id)
 				json-body (json/parse-string (:body response))
 				ciphertext (get json-body "ciphertext")]
-		(send-event ciphertext)
+		(send-event ciphertext correlation-id)
 		ciphertext))
