275355b5719e36a587f8515a498585079030631d
Andy Marks <amarks@thoughtworks.com>
2015-05-24 12:20:02 +1000

    Now sending corr-id to riemann

--- src/normaliser/routes/home.clj
+++ src/normaliser/routes/home.clj
@@ -3,9 +3,9 @@
   			[normaliser.models.core :as model]
             [normaliser.views.layout :as layout]))
 
-(defn home [plaintext]
-  (layout/json-response {:normalised-text (model/normalise plaintext "")}))
+(defn home [plaintext corr-id]
+  (layout/json-response {:normalised-text (model/normalise plaintext corr-id)}))
 
 (defroutes home-routes
-  (GET  "/:plaintext" [plaintext] (home plaintext))
-  (GET  "/" [] (home "")))
+  (GET  "/:plaintext" [plaintext :as request] (home plaintext (get-in request [:headers "x-correlation-id"])))
+  (GET  "/" [] (home "" "")))
