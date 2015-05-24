285d083096fd4484054e876c8fea8ae405c17240
Andy Marks <amarks@thoughtworks.com>
2015-05-24 12:20:07 +1000

    Now sending corr-id to riemann

--- src/square_sizer/routes/home.clj
+++ src/square_sizer/routes/home.clj
@@ -3,10 +3,10 @@
   			[square-sizer.models.core :as model]
             [square-sizer.views.layout :as layout]))
 
-(defn home [plaintext]
-  (layout/json-response {:size (model/square-size plaintext "")}))
+(defn home [plaintext corr-id]
+  (layout/json-response {:size (model/square-size plaintext corr-id)}))
 
 (defroutes home-routes
-  (GET  "/:plaintext" [plaintext] (home plaintext))
-  (GET  "/"           []          (home ""))
+  (GET  "/:plaintext" [plaintext :as request] (home plaintext (get-in request [:headers "x-correlation-id"])))
+  (GET  "/"           []                      (home "" ""))
   )
