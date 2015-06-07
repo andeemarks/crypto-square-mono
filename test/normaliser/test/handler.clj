4f749be499dbabb19e4876ba77c2601d83a882ce
Andy Marks <amarks@thoughtworks.com>
2015-06-07 20:08:02 +1000

    Helper for response status

--- test/normaliser/test/handler.clj
+++ test/normaliser/test/handler.clj
@@ -5,33 +5,36 @@
         cheshire.core
         ring.util.codec))
 
+(defn- success-response [response]
+  (= (:status response) 200))
+
 (deftest test-app
   (testing "gracefully handles no input"
     (let [response (app (request :get "/"))
           body (parse-string (:body response))]
-      (is (= (:status response) 200))
+      (is (success-response response))
       (is (.equals (get body "normalised-text") ""))))
 
   (testing "already normal text is unchanged"
     (let [response (app (request :get "/abcd1234"))
           body (parse-string (:body response))]
-      (is (= (:status response) 200))
+      (is (success-response response))
       (is (.equals (get body "normalised-text") "abcd1234"))))
 
   (testing "everything is down cased"
     (let [response (app (request :get "/aBcDe"))
           body (parse-string (:body response))]
-      (is (= (:status response) 200))
+      (is (success-response response))
       (is (.equals (get body "normalised-text") "abcde"))))
 
   (testing "punctuation is removed"
     (let [response (app (request :get (str "/" (url-encode "a{}bcd']12#$%3&4"))))
           body (parse-string (:body response))]
-      (is (= (:status response) 200))
+      (is (success-response response))
       (is (.equals (get body "normalised-text") "abcd1234"))))
 
   (testing "spaces are removed"
     (let [response (app (request :get "/a+b+c+d"))
           body (parse-string (:body response))]
-      (is (= (:status response) 200))
+      (is (success-response response))
       (is (.equals (get body "normalised-text") "abcd")))))
