85e5c5f85268c715716235490a2a6d891bd5e164
Andy Marks <amarks@thoughtworks.com>
2015-06-07 20:14:55 +1000

    Helper for ciphertext assertion

--- test/normaliser/test/handler.clj
+++ test/normaliser/test/handler.clj
@@ -8,33 +8,31 @@
 (defn- success-response [response]
   (= (:status response) 200))
 
+(defn- expect-cipher-to-be [response ciphertext]
+  (.equals (get (parse-string (:body response)) "normalised-text") ciphertext))
+
 (deftest test-app
   (testing "gracefully handles no input"
-    (let [response (app (request :get "/"))
-          body (parse-string (:body response))]
+    (let [response (app (request :get "/"))]
       (is (success-response response))
-      (is (.equals (get body "normalised-text") ""))))
+      (is (expect-cipher-to-be response ""))))
 
   (testing "already normal text is unchanged"
-    (let [response (app (request :get "/abcd1234"))
-          body (parse-string (:body response))]
+    (let [response (app (request :get "/abcd1234"))]
       (is (success-response response))
-      (is (.equals (get body "normalised-text") "abcd1234"))))
+      (is (expect-cipher-to-be response "abcd1234"))))
 
   (testing "everything is down cased"
-    (let [response (app (request :get "/aBcDe"))
-          body (parse-string (:body response))]
+    (let [response (app (request :get "/aBcDe"))]
       (is (success-response response))
-      (is (.equals (get body "normalised-text") "abcde"))))
+      (is (expect-cipher-to-be response "abcde"))))
 
   (testing "punctuation is removed"
-    (let [response (app (request :get (str "/" (url-encode "a{}bcd']12#$%3&4"))))
-          body (parse-string (:body response))]
+    (let [response (app (request :get (str "/" (url-encode "a{}bcd']12#$%3&4"))))]
       (is (success-response response))
-      (is (.equals (get body "normalised-text") "abcd1234"))))
+      (is (expect-cipher-to-be response "abcd1234"))))
 
   (testing "spaces are removed"
-    (let [response (app (request :get "/a+b+c+d"))
-          body (parse-string (:body response))]
+    (let [response (app (request :get "/a+b+c+d"))]
       (is (success-response response))
-      (is (.equals (get body "normalised-text") "abcd")))))
+      (is (expect-cipher-to-be response "abcd")))))
