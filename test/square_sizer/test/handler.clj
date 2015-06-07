c6f6ea5fcfb23607aa182e56e7c90721f4b96404
Andy Marks <amarks@thoughtworks.com>
2015-06-08 09:31:57 +1000

    Test names now with extra INTENT!

--- test/square_sizer/test/handler.clj
+++ test/square_sizer/test/handler.clj
@@ -7,20 +7,20 @@
 
 (against-background [(send-event anything anything) => ..riemann..]
 
-(facts "when handling GETs"
+(facts "about GETs"
   (fact "handles empty input gracefully"
     (let [response (app (request :get "/"))
           body (parse-string (:body response))]
       (:status response) => 200
       (get body "size") => 0))
 
-  (fact "size when plaintext length is exact square"
+  (fact "calculates correct size when plaintext length is exact square"
     (let [response (app (request :get "/abcd"))
           body (parse-string (:body response))]
       (:status response) => 200
       (get body "size") => 2))
 
-  (fact "size when plaintext length is not square"
+  (fact "calculates correct size when plaintext length is not square"
     (let [response (app (request :get "/abcde"))
           body (parse-string (:body response))]
       (:status response) => 200
