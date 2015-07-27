588883841f22493d171f9ab32b6c0aef9a772b40
Andy Marks <amarks@thoughtworks.com>
2015-07-27 20:45:37 +1000

    Update ALL the versions

--- src/crypto_square_be/services/riemann.clj
+++ src/crypto_square_be/services/riemann.clj
@@ -8,7 +8,6 @@
   (try
     (let [c (riemann/tcp-client {:host (env :riemann-host)})]
       (riemann/send-event c
-        {:service "crypto-square-be" :metric (/ elapsed-time 1000000) :state "ok" :description corr-id})
-      (riemann/close-client c))
+        {:service "crypto-square-be" :metric (/ elapsed-time 1000000) :state "ok" :description corr-id}))
     (catch java.io.IOException ex 
       (log/warn "Cannot find Riemann!"))))
