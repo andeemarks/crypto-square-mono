124f3d8da2a632ddadb912b694d40a181e3689a8
Andy Marks <amarks@thoughtworks.com>
2015-08-02 19:43:43 +1000

    Moved backend health check to service

--- src/crypto_square/routes/home.clj
+++ src/crypto_square/routes/home.clj
@@ -15,23 +15,18 @@
 				ciphertext (model/ciphertext plaintext)]
 	  (layout/input-form plaintext ciphertext)))
 
-(health/defhealthcheck "backend-available?" (fn [] (if (== (be/available?) 200)
-                                            (health/healthy "backend is available!")
-                                            (health/unhealthy "backend is unavailable!"))))
-
-
 (health/defhealthcheck "riemann-available?" (fn [] (if (not (riemann/available?))
                                             (health/unhealthy "riemann is unavailable!")
                                             (health/healthy "riemann is available!"))))
 
 (defn health-check []
-	(let [backend-health (health/check backend-available?)
+	(let [backend-health (be/healthcheck)
         riemann-health (health/check riemann-available?)]
 		{:body 
-      {:healthy? (and (.isHealthy backend-health) (.isHealthy riemann-health)) 
+      {:healthy? (and (:healthy? backend-health) (.isHealthy riemann-health)) 
         :services {
           :riemann {:health (.isHealthy riemann-health) :message (.getMessage riemann-health)} 
-          :backend {:health (.isHealthy backend-health) :message (.getMessage backend-health)}}}}))
+          :backend backend-health}}}))
 
 (defroutes home-routes
   (GET  "/"  [] (home))
--- src/crypto_square/services/backend.clj
+++ src/crypto_square/services/backend.clj
@@ -2,6 +2,7 @@
 	(:require 
 		[clj-http.client :as client]
     [environ.core :refer [env]]
+		[metrics.health.core :as health]
 		[cheshire.core :as json]))
 
 (defn- ciphertext-request [plaintext corr-id]
@@ -15,6 +16,14 @@
 (defn available? []
   (try ((client/get (env :backend-url) {:throw-exceptions false}):status)
   (catch Exception e -1)))
+
+(health/defhealthcheck "backend-available?" (fn [] (if (== (available?) 200)
+                                            (health/healthy "backend is available!")
+                                            (health/unhealthy "backend is unavailable!"))))
+
+(defn healthcheck []
+	(let [health (health/check backend-available?)]
+		{:healthy? (.isHealthy health) :message (.getMessage health)}))
 
 (defn encrypt [plaintext corr-id]
 	(get 
