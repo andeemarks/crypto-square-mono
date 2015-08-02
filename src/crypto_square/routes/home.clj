f75ab5120885d3a8156d1fd081aeb8a71ca0c8ea
Andy Marks <amarks@thoughtworks.com>
2015-08-02 11:48:55 +1000

    First cut of dependency sniff in health check

--- src/crypto_square/routes/home.clj
+++ src/crypto_square/routes/home.clj
@@ -4,6 +4,8 @@
             [crypto-square.views.layout :as layout]
             [prometheus.core :as prometheus]
             [clojure.reflect :as r]
+            [clj-http.client :as client]
+            [environ.core :refer [env]]
             [crypto-square.models.core :as model]))
 
 (defn home []
@@ -14,10 +16,14 @@
 				ciphertext (model/ciphertext plaintext)]
 	  (layout/input-form plaintext ciphertext)))
 
-(health/defhealthcheck "healthy" (fn [] (let [now (.getSeconds (java.util.Date.))]
-                                         (if (< now 30)
-                                            (health/healthy "%d is less than 30!" now)
-                                            (health/unhealthy "%d is more than 30!" now)))))
+(defn http-get [address]
+  (try ((client/get address {:throw-exceptions false}):status)
+  (catch Exception e -1)))
+
+(health/defhealthcheck "healthy" (fn [] (let [status (http-get (env :backend-url))]
+                                         (if (== status 200)
+                                            (health/healthy "%s is available!" (env :backend-url))
+                                            (health/unhealthy "%s is unavailable!" (env :backend-url))))))
 
 (defn health-check []
 	(let [results (health/check healthy)]
