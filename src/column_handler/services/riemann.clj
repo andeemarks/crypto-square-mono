00137a81e66ab79cf6f1ee67c007c5c943184568
Andy Marks <amarks@thoughtworks.com>
2015-11-17 14:44:11 +1100

    Can now failover gracefully with no Riemann

--- src/column_handler/services/riemann.clj
+++ src/column_handler/services/riemann.clj
@@ -8,5 +8,7 @@
     (let [c (riemann/tcp-client {:host (env :riemann-url)})]
       (riemann/send-event c
         {:service "column-handler" :metric (/ elapsed-time 1000000) :state "ok" :description corr-id}))
+    (catch java.lang.Exception ex
+      (log/warn (str "Error connecting to Riemann using config:" (env :riemann-host))))
     (catch java.io.IOException ex 
-      (log/warn "Cannot find Riemann!"))))
+      (log/warn (str "Cannot find Riemann at: " (env :riemann-host))))))
