d87f774772bc4da82ea328fbd38fe399bf4a9427
Andy Marks <amarks@thoughtworks.com>
2015-11-18 08:36:52 +1100

    Now using Docker Compose links to find downstream services

--- up.sh
+++ up.sh
@@ -2,8 +2,8 @@
 : ${PORT:=4000}
 echo Starting server on port $PORT
 export RIEMANN_HOST=127.0.0.1 
-export SQUARE_SIZER_URL=http://192.168.59.103:4002
-export NORMALISER_URL=http://192.168.59.103:4001
-export COLUMN_HANDLER_URL=http://192.168.59.103:4003
+export SQUARE_SIZER_URL=http://square-sizer
+export NORMALISER_URL=http://normaliser
+export COLUMN_HANDLER_URL=http://column-handler
 lein ring server-headless $PORT
 
