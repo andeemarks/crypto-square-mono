4dab35fc347581170b8f654a8f7e163cbb558a5e
Andy Marks <amarks@thoughtworks.com>
2015-11-17 15:05:01 +1100

    Adding failover for missing Riemann

--- src/crypto_square_be/services/riemann.clj
+++ src/crypto_square_be/services/riemann.clj
@@ -25,5 +25,7 @@
     (let [c (riemann/tcp-client {:host (env :riemann-host)})]
       (riemann/send-event c
         {:service "crypto-square-be" :metric (/ elapsed-time 1000000) :state "ok" :description corr-id}))
+    (catch java.lang.Exception ex
+      (log/warn (str "Error connecting to Riemann using config:" (env :riemann-host))))
     (catch java.io.IOException ex 
-      (log/warn "Cannot find Riemann!"))))
+      (log/warn (str "Cannot find Riemann at: " (env :riemann-host))))))
