986eebbb7dee4b2f8c79452409a44541fe9246eb
Andy Marks <vampwillow@gmail.com>
2017-12-11 21:29:50 +1100

    Bumped dep versions up-to-date from lein ancient report - errors with Clojure 1.9 though

--- square-sizer/project.clj
+++ square-sizer/project.clj
@@ -1,16 +1,16 @@
 (defproject square-sizer "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [compojure "1.4.0"]
-                 [cheshire "5.5.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [compojure "1.6.0"]
+                 [cheshire "5.8.0"]
                  [ring.middleware.logger "0.5.0"]
-                 [org.clojure/tools.logging "0.3.1"]
-                 [metrics-clojure "2.5.1"]
-                 [metrics-clojure-health "2.5.1"]
-                 [ring/ring-json "0.3.1"]
-                 [environ "1.0.0"]
-                 [ring-server "0.4.0"]]
+                 [org.clojure/tools.logging "0.4.0"]
+                 [metrics-clojure "2.10.0"]
+                 [metrics-clojure-health "2.10.0"]
+                 [ring/ring-json "0.4.0"]
+                 [environ "1.1.0"]
+                 [ring-server "0.5.0"]]
   :plugins [[lein-ring "0.9.6"]]
   :ring {:handler square-sizer.handler/app
          :init square-sizer.handler/init
@@ -22,7 +22,7 @@
    {:ring
     {:open-browser? false, :stacktraces? false, :auto-reload? false}}
    :dev
-   {:dependencies [ [midje "1.7.0"]
+   {:dependencies [ [midje "1.9.0"]
                     [ring-mock "0.1.5"] 
-                    [ring/ring-devel "1.4.0"]]
+                    [ring/ring-devel "1.6.3"]]
     :plugins [[lein-midje "3.1.3"]]}})
