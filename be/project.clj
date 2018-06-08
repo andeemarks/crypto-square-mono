cd137e67381ac6c916ec70768a34a619c3b171e9
Andy Marks <vampwillow@gmail.com>
2018-06-08 13:58:56 +1000

    Lein ancient update

--- be/project.clj
+++ be/project.clj
@@ -2,10 +2,10 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [compojure "1.6.0"]
+                 [compojure "1.6.1"]
                  [cheshire "5.8.0"]
-                 [clj-http "3.7.0"]
-                 [org.clojure/tools.logging "0.4.0"]
+                 [clj-http "3.9.0"]
+                 [org.clojure/tools.logging "0.4.1"]
                  [metrics-clojure "2.10.0"]
                  [metrics-clojure-health "2.10.0"]
                  [environ "1.1.0"]
@@ -22,7 +22,7 @@
    {:ring
     {:open-browser? false, :stacktraces? false, :auto-reload? false}}
    :dev
-   {:dependencies [ [midje "1.9.0"]
+   {:dependencies [ [midje "1.9.1"]
                     [ring-mock "0.1.5"] 
                     [ring/ring-devel "1.6.3"]]
     :plugins [[lein-midje "3.0.0"]]}})
--- fe/project.clj
+++ fe/project.clj
@@ -1,12 +1,12 @@
 (defproject crypto-square "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [compojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [compojure "1.6.1"]
                  [hiccup "1.0.5"]
-                 [clj-http "3.7.0"]
-                 [ring.middleware.logger "0.5.0"]
-                 [org.clojure/tools.logging "0.4.0"]
+                 [clj-http "3.9.0"]
+                 [ring.middleware.logger "0.5.0" :exclusions [log4j]]
+                 [org.clojure/tools.logging "0.4.1"]
                  [metrics-clojure "2.10.0"]
                  [metrics-clojure-ring "2.10.0"]
                  [metrics-clojure-health "2.10.0"]
@@ -27,10 +27,16 @@
     {:open-browser? false, :stacktraces? false, :auto-reload? false}}
    :dev
    {:dependencies [
-      [clj-webdriver "0.7.2" :exclusions [org.seleniumhq.selenium/selenium-server]]
-      [org.seleniumhq.selenium/selenium-server "3.8.1"]      
-      [ring-mock "0.1.5"] 
-      [midje "1.9.0"]
-      [ring/ring-jetty-adapter "1.6.3"]
-      [ring/ring-devel "1.6.3"]]
+                   [clj-webdriver "0.7.2" :exclusions [org.seleniumhq.selenium/selenium-server]]
+                   [org.seleniumhq.selenium/selenium-server "3.12.0" :exclusions 
+                    [[org.eclipse.jetty/jetty-client]
+                     [org.eclipse.jetty/jetty-xml]
+                     [org.eclipse.jetty.websocket/websocket-api]
+                     [org.eclipse.jetty.websocket/websocket-client]
+                     [org.eclipse.jetty.websocket/websocket-common]
+                     [org.apache.commons/commons-text]]]
+                   [ring-mock "0.1.5"] 
+                   [midje "1.9.1"]
+                   [ring/ring-jetty-adapter "1.6.3"]
+                   [ring/ring-devel "1.6.3"]]
     :plugins [[lein-midje "3.1.3"]]}})
