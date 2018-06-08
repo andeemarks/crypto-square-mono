06cc5cafe589d4a9d3b14070502e5f7474f990d2
Andy Marks <vampwillow@gmail.com>
2018-06-08 14:40:41 +1000

    Now using Clojure 1.9

--- be/project.clj
+++ be/project.clj
@@ -1,13 +1,14 @@
 (defproject crypto-square-be "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [compojure "1.6.1"]
                  [cheshire "5.8.0"]
                  [clj-http "3.9.0"]
                  [org.clojure/tools.logging "0.4.1"]
-                 [metrics-clojure "2.10.0"]
-                 [metrics-clojure-health "2.10.0"]
+                 [onelog "0.5.0" :exclusions [io.aviso/pretty]]
+                 [metrics-clojure "2.10.0"  :exclusions [org.slf4j/slf4j-api]]
+                 [metrics-clojure-health "2.10.0"  :exclusions [org.slf4j/slf4j-api]]
                  [environ "1.1.0"]
                  [ring/ring-json "0.4.0"]
                  [ring.middleware.logger "0.5.0"]
