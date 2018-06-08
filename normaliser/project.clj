0f17c2f7ba0a7e7331e832fe6f89a748465a804a
Andy Marks <vampwillow@gmail.com>
2018-06-08 14:02:43 +1000

    Lein ancient update

--- normaliser/project.clj
+++ normaliser/project.clj
@@ -2,8 +2,8 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [compojure "1.6.0"]
-                 [org.clojure/tools.logging "0.4.0"]
+                 [compojure "1.6.1"]
+                 [org.clojure/tools.logging "0.4.1"]
                  [ring.middleware.logger "0.5.0"]
                  [environ "1.1.0"]
                  [metrics-clojure "2.10.0"]
@@ -21,8 +21,8 @@
    {:ring
     {:open-browser? false, :stacktraces? false, :auto-reload? false}}
    :dev
-   {:dependencies [ [ring/ring-codec "1.1.0"]
-                    [midje "1.9.0"] 
+   {:dependencies [ [ring/ring-codec "1.1.1"]
+                    [midje "1.9.1"] 
                     [ring-mock "0.1.5"] 
                     [ring/ring-devel "1.6.3"]]
     :plugins [[lein-midje "3.1.3"]]}})
