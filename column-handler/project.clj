d5ec2a02370354712d12812ecb27d951d6a058d4
Andy Marks <vampwillow@gmail.com>
2018-06-08 14:07:09 +1000

    Lein ancient update

--- column-handler/project.clj
+++ column-handler/project.clj
@@ -2,7 +2,7 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [compojure "1.6.0"]
+                 [compojure "1.6.1"]
                  [ring/ring-json "0.4.0"]
                  [metrics-clojure "2.10.0"]
                  [metrics-clojure-health "2.10.0"]
@@ -20,7 +20,7 @@
    {:ring
     {:open-browser? false, :stacktraces? false, :auto-reload? false}}
    :dev
-    {:dependencies [ [midje "1.9.0"]
+    {:dependencies [ [midje "1.9.1"]
                      [ring-mock "0.1.5"] 
                      [ring/ring-devel "1.6.3"]]
      :plugins [[lein-midje "3.1.3"]]}})
