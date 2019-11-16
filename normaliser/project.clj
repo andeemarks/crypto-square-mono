e391f08adf2c1eef2f1acdf65c322ffcb13c6ee0
Andy Marks <vampwillow@gmail.com>
2019-11-17 09:58:45 +1100

    Dep freshen

--- normaliser/project.clj
+++ normaliser/project.clj
@@ -1,17 +1,17 @@
 (defproject normaliser "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :dependencies [[org.clojure/clojure "1.10.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [compojure "1.6.1"]
-                 [metosin/compojure-api "1.1.12"]
-                 [org.clojure/tools.logging "0.4.1"]
+                 [metosin/compojure-api "1.1.13"]
+                 [org.clojure/tools.logging "0.5.0"]
                  [ring.middleware.logger "0.5.0"]
                  [environ "1.1.0"]
                  [metrics-clojure "2.10.0"  :exclusions [org.slf4j/slf4j-api]]
                  [metrics-clojure-health "2.10.0"  :exclusions [org.slf4j/slf4j-api]]
-                 [ring/ring-json "0.4.0"]
+                 [ring/ring-json "0.5.0"]
                  [ring-server "0.5.0"]]
-  :plugins [[lein-ring "0.9.6"]]
+  :plugins [[lein-ring "0.12.5"]]
   :ring {:handler normaliser.handler/app
          :init normaliser.handler/init
          :destroy normaliser.handler/destroy}
@@ -22,6 +22,6 @@
    {:ring
     {:open-browser? false, :stacktraces? false, :auto-reload? false}}
    :dev
-   {:dependencies [[ring/ring-codec "1.1.1"]
+   {:dependencies [[ring/ring-codec "1.1.2"]
                    [ring-mock "0.1.5"]
-                   [ring/ring-devel "1.7.1"]]}})
+                   [ring/ring-devel "1.8.0"]]}})
