79ffd46e2748c888e7be7c8161d92f02647bee55
Andy Marks <vampwillow@gmail.com>
2019-11-17 10:01:21 +1100

    Dep freshen

--- be/project.clj
+++ be/project.clj
@@ -1,20 +1,20 @@
 (defproject crypto-square-be "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :dependencies [[org.clojure/clojure "1.10.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [compojure "1.6.1"]
-                 [metosin/compojure-api "1.1.11"]
-                 [cheshire "5.8.1"]
-                 [clj-http "3.9.1"]
-                 [org.clojure/tools.logging "0.4.1"]
+                 [metosin/compojure-api "1.1.13"]
+                 [cheshire "5.9.0"]
+                 [clj-http "3.10.0"]
+                 [org.clojure/tools.logging "0.5.0"]
                  [onelog "0.5.0" :exclusions [io.aviso/pretty]]
                  [metrics-clojure "2.10.0"  :exclusions [org.slf4j/slf4j-api]]
                  [metrics-clojure-health "2.10.0"  :exclusions [org.slf4j/slf4j-api]]
                  [environ "1.1.0"]
-                 [ring/ring-json "0.4.0"]
+                 [ring/ring-json "0.5.0"]
                  [ring.middleware.logger "0.5.0"]
                  [ring-server "0.5.0"]]
-  :plugins [[lein-ring "0.9.6"]]
+  :plugins [[lein-ring "0.12.5"]]
   :ring {:handler crypto-square-be.handler/app
          :init crypto-square-be.handler/init
          :destroy crypto-square-be.handler/destroy}
@@ -25,5 +25,5 @@
     {:open-browser? false, :stacktraces? false, :auto-reload? false}}
    :dev
    {:dependencies [[ring-mock "0.1.5"]
-                   [clj-fakes "0.11.0"]
-                   [ring/ring-devel "1.7.1"]]}})
+                   [clj-fakes "0.12.0"]
+                   [ring/ring-devel "1.8.0"]]}})
