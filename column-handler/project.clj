7647c59f50f96906b7258bd68e69e10e000408eb
Andy Marks <vampwillow@gmail.com>
2019-11-17 09:53:32 +1100

    Dep freshen

--- column-handler/project.clj
+++ column-handler/project.clj
@@ -1,20 +1,20 @@
 (defproject column-handler "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :dependencies [[org.clojure/clojure "1.10.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [compojure "1.6.1"]
-                 [ring/ring-json "0.4.0"]
-                 [metosin/compojure-api "1.1.11"]
+                 [ring/ring-json "0.5.0"]
+                 [metosin/compojure-api "1.1.13"]
                  [metrics-clojure "2.10.0" :exclusions [org.slf4j/slf4j-api]]
                  [metrics-clojure-health "2.10.0" :exclusions [org.slf4j/slf4j-api]]
                  [environ "1.1.0"]
                  [ring.middleware.logger "0.5.0"]
                  [ring-server "0.5.0"]]
-  :plugins [[lein-ring "0.9.6"]]
+  :plugins [[lein-ring "0.12.5"]]
   :ring {:handler column-handler.handler/app
          :init column-handler.handler/init
          :destroy column-handler.handler/destroy}
-  :min-lein-version "2.0.0"
+  :min-lein-version "2.8.0"
   :profiles
   {:uberjar {:aot :all}
    :production
@@ -22,4 +22,4 @@
     {:open-browser? false, :stacktraces? false, :auto-reload? false}}
    :dev
    {:dependencies [[ring-mock "0.1.5"]
-                   [ring/ring-devel "1.7.1"]]}})
+                   [ring/ring-devel "1.8.0"]]}})
